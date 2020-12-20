d92db7da09d4f7b8b1595c96584c764213eab4a5
Shiru99 <shriram.ghadge1999@gmail.com>
2020-12-20 12:59:43 +0530

    images adding

--- EventManager/lib/Classes/GuestInfo.dart
+++ EventManager/lib/Classes/GuestInfo.dart
@@ -4,7 +4,7 @@ class GuestInfo{
 
   // String imageURL = "https://parsec.iitdh.ac.in/images/logos/logo-about.jpg";
 
-  String imageURL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcomputers.tutsplus.com%2Ftutorials%2Fusing-the-guest-account-in-os-x--mac-45793&psig=AOvVaw2MtJi0YexgVnnC0QjSzl9o&ust=1608533703697000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJCewZL92-0CFQAAAAAdAAAAABAD";
+  String imageURL = "https://github.com/Shiru99/Event-Management-System/blob/main/EventManager/assets/images/Guest.png";
 
   
 
